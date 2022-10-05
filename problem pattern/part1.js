4b02c7649088b5b07557f08a75b365412e0c3571
jinhee kim <wlsgml0229@naver.com>
2022-10-05 23:52:17 +0900

    빈도수세기 2

--- problem pattern/part1.js
+++ problem pattern/part1.js
@@ -3,7 +3,7 @@ function smae(arr1, arr2) {
     return false;
   }
   for (let i = 0; i < arr1.length; i++) {
-    let coorectIndex = arr.indexOf(arr[i] ** 2);
+    let coorectIndex = arr.indexOf(arr1[i] ** 2);
     if (coorectIndex === -1) {
       return false;
     }
@@ -12,4 +12,32 @@ function smae(arr1, arr2) {
   return true;
 }
 
-[1, 2, 3], [9, 1, 4, 4];
+smae([1, 2, 3, 2], [9, 1, 4, 4]);
+
+// O(n^2) //순진한 접근법
+// 중첩된 루프는 사용하지 않는게좋다.
+
+// O(n) 시간
+// 대략 3n 의 시간이 걸리며 위의 방법보다는 낫다.
+function same1(arr1, arr2) {
+  if (arr.length !== arr2.length) {
+    return false;
+  }
+  let frequencyCounter1 = {};
+  let frequecyCounter2 = {};
+  for (let val of arr1) {
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+  }
+  for (let val of arr2) {
+    frequecyCounter2[val] = (frequecyCounter2[val] || 0) + 1;
+  }
+  for (let key in frequencyCounter1) {
+    if (!(key ** 2 in frequecyCounter2)) {
+      return false;
+    }
+    if (frequecyCounter2[key ** 2] !== frequencyCounter1[key]) {
+      return false;
+    }
+    return true;
+  }
+}
