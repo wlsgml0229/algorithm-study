19c456698cf7a4e9480330cd1488ec7a87291d1f
jinhee kim <wlsgml0229@naver.com>
2022-12-16 23:59:49 +0900

    재귀 연습문제 추가

--- recursion/test/test1.js
+++ recursion/test/test1.js
@@ -11,3 +11,23 @@ function factorial(x) {
   if (x <= 1) return 1;
   return x * factorial(x - 1);
 }
+
+//Write a function called recursiveRange which accepts a number and adds up all the numbers from 0 to the number passed to the function
+function recursiveRange(n) {
+  if (n <= 1) return 1;
+  return n + recursiveRange(n - 1);
+}
+/**
+ * 반복 , 또는 돌면서 한글자씩 저장되어야하고
+ * 종료조건: 글자의 길이가 하나가 남으면 return
+ * toUpperCase() 메서드는 문자열을 대문자로 변환
+ */
+
+function capitalizeWords(arr) {
+  if (arr.length === 1) {
+    return [arr[0].toUppercase()];
+  }
+  let res = capitalizeWords(arr.slice(0, -1));
+  res.push(arr.slice(arr.length - 1)[0].toUppercase());
+  return res;
+}
