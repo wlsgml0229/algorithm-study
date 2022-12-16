b1dabb45a4836a1fe1023af8607f955a0f5fbfad
jinhee kim <wlsgml0229@naver.com>
2022-12-16 22:05:07 +0900

    재귀문제풀이2

--- recursion/test/test1.js
+++ recursion/test/test1.js
@@ -5,3 +5,9 @@ function power(base, exponent) {
   if (exponent === 0) return 1;
   return base * power(base, exponent - 1);
 }
+
+function factorial(x) {
+  if (x < 0) return 0;
+  if (x <= 1) return 1;
+  return x * factorial(x - 1);
+}
