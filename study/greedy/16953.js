a7739799c0f7dbd74a7bc8149c666b28556aae28
jinhee kim <wlsgml0229@naver.com>
2023-06-25 19:27:01 +0900

    그리디 - 백준 16953

---
+++ study/greedy/16953.js
@@ -0,0 +1,26 @@
+let input = require("fs").readFileSync("input.txt").toString();
+
+// 2를 곱한다
+// 1을 오른쪽에 더해준다
+// a를 조작하여 b를 만들기
+// 불가능하다면 -1 출력
+// a 가 b 보다 커지면 만들수없음
+
+let [a, b] = input.split(" ").map(Number);
+// 가장처음 연산 한번 존재
+let cnt = 1;
+let flag = false;
+
+while (a <= b) {
+  if (a === b) {
+    flag = true;
+    break;
+  }
+  if (b % 2 === 0) parseInt(b / 2);
+  else if (b % 10 === 1) parseInt(b % 10);
+  else break;
+  cnt++;
+}
+
+if (flag) console.log(cnt);
+else console.log(-1);
