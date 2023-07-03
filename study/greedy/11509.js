64342bea60d4488e0a335bbd94c776c93221a91f
wlsgml0229 <wlsgml0229@naver.com>
2023-07-03 12:49:02 +0900

    그리디 - 11509 주석추가

--- study/greedy/11509.js
+++ study/greedy/11509.js
@@ -6,15 +6,15 @@ const [n, rest] = require('fs').readFileSync(file).toString().trim().split('\n')
 let data = rest.split(' ').map(Number);
 let result = 0;
 // 0 으로 채운 배열에 해당 인덱스에 화살이 있는 지 확인 후 없으면 화살 추가 있으면 화살 쓴다.
-let arrow = new Array(1000001).fill(0);
+let arrow = new Array(1000001).fill(0); // 각 높이에 화살이 몇개 있는지 확인할 배열 0으로 셋팅
 for(let x of data) {
-    if(arrow[x] > 0) {
+    if(arrow[x] > 0) { // 해당높이에 화살이 있다면
         arrow[x] -= 1;
         arrow[x-1] += 1;
     }
     else {
-        arrow[x-1] += 1;
-        result += 1;
+        arrow[x-1] += 1; // h-1 추가
+        result += 1; // 화살 쏘기
     }
 }
 console.log(result);
