8a9545dea424839ec2a5361669c01118dd816a8f
wlsgml0229 <wlsgml0229@naver.com>
2023-08-07 16:57:13 +0900

    DFS 1240

---
+++ study/dfs/1240.js
@@ -0,0 +1,34 @@
+const file = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+
+const input = require('fs').readFileSync(file).toString().split('\n')
+
+let [n,m] = input[0].split(' ').map(Number);
+// 트리의 정보 입력
+let graph = [];
+for(let i = 1; i <= n; i++) graph[i] = [];
+for(let i = 1; i < n; i++) {
+    let [x,y, cost] = input[i].split(' ').map(Number);
+    graph[x].push([y, cost]);
+    graph[y].push([x, cost]);
+}
+
+function dfs (x, dist) {
+    if(visited[x]) return;
+    visited[x] = true;
+    distance[x] = dist;
+    for(let [y,cost] of graph[x]) dfs(y, dist+ cost)
+}
+
+let distance;
+let visited;
+for (let i = 0; i < m; i++) {
+    let [x, y] = input[n + i].split(' ').map(Number);
+    visited = new Array(n + 1).fill(false)
+    distance = new Array(n + 1).fill(-1);
+    dfs(x, 0)
+    console.log(distance[y])
+}
+
+
+
+
